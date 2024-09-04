0f498cc5a09ddda382b0dbad55449ae8ea415b29
Ennefai Mohcine <ayman.widadi.1420@gmail.com>
2024-09-04 18:11:38 +0100

    Add increment feature

--- App.js
+++ App.js
@@ -1,20 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
-export default function App() {
+const App = () => {
+  const [x, setX] = React.useState(0);
+  function onIncrement() {
+    setX(x + 1);
+  }
+  function onDecrement() {
+    setX(x - 1);
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <Text style={styles.text}>
+          Hello,{"\n"}  
+          welcome to the application increment counter {"\n"}
+          Count: {x}
+        </Text>
+        <TouchableOpacity style={styles.button} onPress={onIncrement}>
+          <Text style={styles.buttonText}>Click Here to Increment</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button1} onPress={onDecrement}>
+          <Text style={styles.buttonText}>Click Here to Increment</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  safeArea: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  },
+  container: {
+    marginLeft: "10%",
+    marginTop: "50%",
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 18,
+    textAlign: 'center',
+  },
+  button: {
+    backgroundColor: "green",
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 20,
+  },
+  button1: {
+    backgroundColor: "red",
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 20,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
   },
 });
+
+export default App;
